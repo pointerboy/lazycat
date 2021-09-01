7d1d37607fc515e4c0ec6e9194b3a7800bd4e2e7
pointerboy <lukatrbovic@outlook.com>
2021-09-01 18:05:23 +0200

    Added WebConsole files

---
+++ WebConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using LightWebServer;
+
+namespace WebConsole
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Server.SpawnServer(8000);
+            Console.ReadKey();
+        }
+    }
+}
