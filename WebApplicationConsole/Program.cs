9c0a3b58ff20105428d40e90630ac012eed22c9b
pointerboy <lukatrbovic@outlook.com>
2021-09-05 22:37:01 +0200

    Added basic web routing alongside render template function

---
+++ WebApplicationConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace WebApplicationConsole
+{
+    internal class Program
+    {
+        public static WebServer.WebServer server = new WebServer.WebServer();
+        
+        public static void Main(string[] args)
+        {
+            server.AddWebRoute("Index", IndexPage);
+            server.AddWebRoute("Test", Test);
+            server.Start(@"C:\Users\Luka\RiderProjects\LightWebServer\WebApplicationConsole\Web", 8084);
+
+            Console.ReadKey();
+        }
+
+        private static void Test()
+        {
+            server.RenderWebTemplate(@"C:\Users\Luka\RiderProjects\LightWebServer\WebApplicationConsole\Web\test.html");
+        }
+
+        private static void IndexPage()
+        {
+            string hello_world = "Hello_World!";
+            server.RenderWebTemplate(@"C:\Users\Luka\RiderProjects\LightWebServer\WebApplicationConsole\Web\index.html", hello_world);
+        }
+    }
+}
